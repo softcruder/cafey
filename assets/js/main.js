85fc7587080ad9eb213b7e0c75257b6e19ca7b57
Nasirullah Oladipo <60460558+softcruder@users.noreply.github.com>
2022-11-02 00:37:46 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+/*=============== LOADER ===============*/
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* ===== MENU SHOW ====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* ===== MENU SHOW ====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  //When the scroll is greater than 50 viewport height
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== MIXITUP FILTER PRODUCTS ===============*/
+let mixerProducts = mixitup('.products__content', {
+    selectors: {
+        target: '.products__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Default filter products */
+mixerProducts.filter('.delicacies');
+
+/* Link active products */
+const linkProducts = document.querySelectorAll('.products__item');
+
+function activeProduct() {
+    linkProducts.forEach(l=> l.classList.remove('active-product'))
+    this.classList.add('active-product')
+};
+
+linkProducts.forEach(l=> l.addEventListener('click', activeProduct));
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
